09dd05758826c334e8a037447b37cf656e785a19
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-03-22 06:51:35 -0300

    background added

---
+++ js/age.js
@@ -0,0 +1,15 @@
+const dob = new Date('03/16/1997');
+// calculate month difference from current date in time  
+const month_diff = Date.now() - dob.getTime();
+
+// convert the calculated difference in date format  
+const age_dt = new Date(month_diff);
+
+// extract year from date      
+const year = age_dt.getUTCFullYear();
+
+// now calculate the age of the user  
+const age = Math.abs(year - 1970);
+
+// display the calculated age  
+document.getElementById('age').innerText = age;  
