2c13da7f75bac2c91b9302950c064dc6f77579f3
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-08-13 02:35:34 -0300

    resize - reset stars position

--- js/night.js
+++ js/night.js
@@ -9,19 +9,24 @@ function initNight() {
         return Math.floor(Math.random() * (max - min)) + min;
     }
 
-    let star = "";
-    const amountStars = 500;
-    let night = document.querySelector(".stars");
-    const widthWindow = window.innerWidth;
-    const heightWindow = window.innerHeight;
+    const loadStars = () => {
+        let star = "";
+        const amountStars = 500;
+        let night = document.querySelector(".stars");
+        const widthWindow = window.innerWidth;
+        const heightWindow = window.innerHeight;
 
-    for (let i = 0; i < amountStars; i++) {
-        star += "<span class='star " + style[getRandomArbitrary(0, 4)] + " " + opacity[getRandomArbitrary(0, 6)] + " "
-            + tam[getRandomArbitrary(0, 5)] + "' style='animation-delay: ." + getRandomArbitrary(0, 9) + "s; left: "
-            + getRandomArbitrary(0, widthWindow) + "px; top: " + getRandomArbitrary(0, heightWindow) + "px;'></span>";
-    }
+        for (let i = 0; i < amountStars; i++) {
+            star += "<span class='star " + style[getRandomArbitrary(0, 4)] + " " + opacity[getRandomArbitrary(0, 6)] + " "
+                + tam[getRandomArbitrary(0, 5)] + "' style='animation-delay: ." + getRandomArbitrary(0, 9) + "s; left: "
+                + getRandomArbitrary(0, widthWindow) + "px; top: " + getRandomArbitrary(0, heightWindow) + "px;'></span>";
+        }
 
-    night.innerHTML = star;
+        night.innerHTML = star;
+    };
+
+    loadStars();
+    addEventListener("resize", () => loadStars());
 
     // Meteors
     let randomNumber = 5000;
