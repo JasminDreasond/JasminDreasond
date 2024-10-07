03ec5122b892e7b97997c8df099a406fc0fde997
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-10-07 19:52:23 -0300

    some performance issues are corrected now.

--- js/rainbow.js
+++ js/rainbow.js
@@ -1,26 +1,25 @@
-window.addEventListener("load", function () {
-    let elements = document.getElementsByClassName("rainbowText");
-    for (let i = 0; i < elements.length; i++) {
-        generateRainbowText(elements[i]);
-    }
-});
+window.addEventListener("load", rainbowTextStart);
 
-function generateRainbowText(element) {
-    let tinyCount = 0;
-    const rainbowTime = () => {
-        let text = element.innerText;
-        element.innerHTML = "";
-        for (let i = 0; i < text.length; i++) {
-            let charElem = document.createElement("span");
-            charElem.style.color = "hsl(" + (360 * Number(i + tinyCount) / text.length) + ",80%,50%)";
-            charElem.innerHTML = text[i];
-            element.appendChild(charElem);
-        }
-    };
-    setInterval(() => {
+function generateRainbowText(element, speed = 90) {
+    if (!element.hasAttribute('rainbow-text-enabled')) {
+        element.setAttribute('rainbow-text-enabled', true);
+
+        let tinyCount = 0;
+        const rainbowTime = () => {
+            let text = element.innerText;
+            element.innerHTML = "";
+            for (let i = 0; i < text.length; i++) {
+                let charElem = document.createElement("span");
+                charElem.style.color = "hsl(" + (360 * Number(i + tinyCount) / text.length) + ",80%,50%)";
+                charElem.innerHTML = text[i];
+                element.appendChild(charElem);
+            }
+        };
+        setInterval(() => {
+            rainbowTime();
+            tinyCount++;
+            if (tinyCount > 360) tinyCount = 0;
+        }, speed);
         rainbowTime();
-        tinyCount++;
-        if (tinyCount > 360) tinyCount = 0;
-    }, 90);
-    rainbowTime();
+    }
 }
